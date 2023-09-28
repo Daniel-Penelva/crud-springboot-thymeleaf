f023ab5f76e115f8d491beafe0aeeba4743b53ad
Daniel Penelva <d4n.andrade@gmail.com>
2023-09-27 23:11:17 -0300

    Criando projeto maven spring boot

---
+++ src/main/java/com/daniel/crudspringbootthymeleaf/CrudSpringbootThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.crudspringbootthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringbootThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringbootThymeleafApplication.class, args);
+	}
+
+}
